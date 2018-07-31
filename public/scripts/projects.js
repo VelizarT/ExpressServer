7beb7d060f80db2aef8af96e78bb6b92725d121e
VelizarT <velizar_tonchev@abv.bg>
2018-07-31 23:04:28 +0300

    Added header, home, about, projects, contacts section

--- public/scripts/projects.js
+++
@@ -1,3 +0,0 @@
-$('.test-btn').on('click', () => {
-    alert('Clicked!');
-});
--- server.js
+++ server.js
@@ -8,59 +8,36 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', hbs);
 
-//next - tells the app that the middleware function is completed
-app.use((req, res, next) => {
-    var now = new Date().toString();
-    // console.log(req);
-    var log = `${now}: ${req.method}, ${req.url}`;
-    fs.appendFile('server.log', log + '\n', (err) =>{
-        if(err) {
-            console.log('Unable to append to server.log.');
-        }
-    });
-    next();
-});
-
 // app.use((req, res, next) => {
 //     res.render('maintenance.hbs');
 // });
 
-//express middleware - teaches express how to read from static directory
-//express.static() - absolute path to public folder;
-//__dirname - path to the project directory; server independent; path to ExpressApp in this case
 app.use(express.static(__dirname + '/public')); 
 
 //regiter helper function to use in templates
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
-});
+// hbs.registerHelper('getCurrentYear', () => {
+//     return new Date().getFullYear();
+// });
 
-hbs.registerHelper('screamIt', (text) => {
-    return text.toUpperCase();
-});
+// hbs.registerHelper('screamIt', (text) => {
+//     return text.toUpperCase();
+// });
 
 app.get('/', (req, res) => {
-
-    //var logsString = fs.readFileSync('server.log');
-
-    res.render('home.hbs', {
-        title: 'Home Page',
-        welcomeMessage: 'Welcome to my web site!' ,
-        //logs: logsString
-    });
+    res.render('home.hbs');
 });
 
-app.get('/about', (req, res) => {
-    res.render('about.hbs', {
-        title: 'About Page',
-    });
-});
+// app.get('/about', (req, res) => {
+//     res.render('about.hbs', {
+//         title: 'About Page',
+//     });
+// });
 
-app.get('/projects', (req, res) => {
-    res.render('projects.hbs', {
-        title: 'Projects'
-    });
-});
+// app.get('/projects', (req, res) => {
+//     res.render('projects.hbs', {
+//         title: 'Projects'
+//     });
+// });
 
 //sends back json with error message
 app.get('/bad', (req, res) => {
