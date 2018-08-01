447c0b5e91d43344e3d4879fd1da138db163c2fc
VelizarT <velizar_tonchev@abv.bg>
2018-08-01 21:55:57 +0300

    Favico and scroll functionality added

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
     $('.fixed-action-btn').floatingActionButton();
+
+    $(".home-btn").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".home").offset().top
+        }, 700);
+    });
+
+    $(".about-btn").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".about").offset().top
+        }, 700);
+    });
+
+    $(".projects-btn").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".projects").offset().top+2
+        }, 700);
+    });
+
+    $(".contacts-btn").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".contacts").offset().top+11
+        }, 700);
+    });
+
   });
+  
